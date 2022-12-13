0008e887b7a48b8c272c0ddbf6ae2e5ca53b234e
Francis Villacres <francis.germanico@hotmail.com>
2022-12-13 18:45:29 -0500

    PDE solver created

---
+++ PDE_solvers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 13 12:16:05 2022
+
+@author: franc
+"""
+
+
+
+'''
+Implicit method leads to a system of equation. Question: Why do we not have
+a system of equation when explicit method comes to?
+
+'''
+
